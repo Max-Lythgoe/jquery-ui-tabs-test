dc31bb47ce09587385a6f49d0617378fa42f6c82
Max Lythgoe <LYTHGOEMAX@GMAIL.COM>
2024-07-15 21:34:55 -0600

    Add files via upload

---
+++ jquery-ui-tab-testing/README.md
@@ -0,0 +1,4 @@
+# JQuery UI Tab Testing
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Max-Lythgoe-the-sans/pen/abgvWqY](https://codepen.io/Max-Lythgoe-the-sans/pen/abgvWqY).
+
---
+++ jquery-ui-tab-testing/src/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $("#tabs").tabs({
+        show: { effect: "fadeIn", duration: 300 },
+        hide: { effect: "fadeOut", duration: 300 },
+        activate: function(event, ui) {
+            // Show the image with a different effect when a tab is activated
+            ui.newPanel.find('.tab-image').show('scale', { direction: 'right' }, 500);
+        },
+        beforeActivate: function(event, ui) {
+            // Hide the image with a different effect before a tab is activated
+            ui.oldPanel.find('.tab-image').hide('puff', { direction: 'left' }, 500);
+        }
+    });
+});
+
